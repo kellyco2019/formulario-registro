cd1ef642bffd0407919395e49658273abbbcd05e
Kelly <kellyc@evollve.co>
2020-10-05 21:47:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const mustacheExpress = require ("mustache-express")
+
+const bodyParser = require('body-parser');
+const model = require("./model");
+
+const app = express();
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.engine("html", mustacheExpress());
+
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views")
+
+app.get("/", async (req , res) => {
+   const visitors = await model.getallVisitors();
+   
+   res.render("table.html" , { visitors });
+});
+
+app.get("/register" , (req , res) => {
+    res.render("register.html", {})
+});
+app.post("/register", (req , res) => {
+    const {name , email , password } = req.body
+
+    const user = {name , email , password };
+
+    model.createVisitor(user , () => {
+        res.redirect("/");   
+    });
+});
+
+app.listen(3000 , () => {
+    console.log("server listening on port 3000");
+});
---
+++ model.js
@@ -0,0 +1,45 @@
+const mongoose = require ("mongoose");
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/test', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("error", function (err) {
+    if (err) {
+        return console.error(err);
+    }
+});
+
+const VisitorSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+
+const VisitorModel= mongoose.model("Visitor", VisitorSchema)
+
+async function createVisitor(user, callback) {
+    VisitorModel.create(user, function(err){
+        if (err) {
+            return console.error(err);
+        }
+        callback()
+    });
+}
+
+async function getAllvisitors() {
+    return await VisitorModel.find({}, function (err,visitors ) {
+        if (err) {
+            return console.error(err);
+        }
+        return visitors;
+    });
+}
+
+module.exports = {
+    
+    createVisitor,
+    getAllvisitors,
+};
+
